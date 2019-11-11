0af8749fdebd12fdb0a63d3196402d99eb510abf
yueshengyu <893425091@qq.com>
2019-11-11 10:47:57 +0800

    less 处理

--- src/build-components.js
+++ src/build-components.js
@@ -1,41 +1,25 @@
 //使用webpack对组件进行打包
 
 const yParser = require('yargs-parser');
-const { join, resolve } = require('path');
-const { existsSync, statSync, readdirSync } = require('fs');
+const { join, resolve, extname, basename } = require('path');
+const { existsSync, statSync, readdirSync, copyFileSync } = require('fs-extra');
 const assert = require('assert');
-const log = require('./utils/log');
+const execa = require('execa');
 const slash = require('slash2');
 const chalk = require('chalk');
 const rimraf = require('rimraf');
 const globby = require('globby');
 const webpack = require('webpack');
+const log = require('./utils/log');
+const getWebpackConfig = require('./utils/getWebpackConfig');
 const cwd = process.cwd();
-const postcssPresetEnv = require('postcss-preset-env');
 
-const BabelOption = {
-    presets: [
-        [
-            require.resolve('@babel/preset-typescript'), // ts预设
-            {},
-        ],
-        [
-            require.resolve('@babel/preset-env'), // 官方推荐的env预设
-        ],
-        require.resolve('@babel/preset-react')
-        // ...(isBrowser ? [require.resolve('@babel/preset-react')] : []), // 浏览器环境加入react预设
-    ],
-    plugins: [
-        require.resolve('@babel/plugin-proposal-export-default-from'),
-        require.resolve('@babel/plugin-proposal-do-expressions'),
-        require.resolve('@babel/plugin-proposal-class-properties'),
-    ]
-}
+
 function build(dir, opts = {}) {
     const { cwd } = opts;
     const inputPath = join(cwd, 'components', dir);
     const outputPath = join(cwd, 'antd', dir);
-    const entries = globby.sync(
+    const jsEntries = globby.sync(
         [
             `${inputPath}/**/*.(js|jsx|ts|tsx)`,
             `!${inputPath}/__tests__`,
@@ -45,145 +29,33 @@ function build(dir, opts = {}) {
 
         }
     ).reduce((memo, cur) => {
-        return { ...memo, [cur.replace(`${inputPath}/`, '')]: cur }
+        const ext = extname(cur);
+        return { ...memo, [cur.replace(`${inputPath}/`, '').replace(ext, '')]: cur }
     }, {})
+    function lessHandle() {
+        globby.sync(
+            [
+                `${inputPath}/style/*.less`
+            ]
+        ).forEach(path => {
+            try {
+                //复制
+                const base = basename(path, '.less');
+                const output = `${outputPath}/style/${base}`
+                copyFileSync(path, `${output}.less`)
+                //编译为css
+                execa.commandSync(`less ${path} ${output}.css`)
+            } catch (e) {
+                throw (`less处理出错: ${e}`)
+            }
+        })
+    }
     const compiler = webpack({
-        entry: entries,
-        mode: 'production',
+        ...getWebpackConfig({cwd}),
+        entry: jsEntries,
         output: {
             path: outputPath,
             filename: '[name].js'
-        },
-        module: {
-            rules: [
-                {
-                    test: /\.(js|jsx)$/,
-                    loader: 'babel-loader',
-                    options: BabelOption,
-                    exclude: /node_modules/,
-                },
-                {
-                    test: /\.(ts|tsx)$/,
-                    use: [
-                        {
-                            loader: 'babel-loader',
-                            options: BabelOption,
-                        },
-                        {
-                            loader: 'ts-loader',
-                            options: {
-                                configFile: join(cwd, 'tsconfig.json'),
-                            }
-                        }
-                    ],
-                    exclude: /node_modules/,
-                },
-                {
-                    test: /\.(css|less)$/,
-                    exclude: /node_modules/,
-                    use: [{
-                        loader: 'style-loader'
-                    },
-                    {
-                        loader: 'css-loader',
-                        options: {
-                            sourceMap: true,
-                            modules: { // 适配css-loader2
-                                localIdentName: '[local].[hash:8]'
-                            },
-                        }
-                    },
-                    {
-                        loader: 'postcss-loader',
-                        options: {
-                            ident: 'postcss',
-                            plugins: () => [postcssPresetEnv()]
-                        }
-                    },
-                    {
-                        loader: 'less-loader',
-                        options: {
-                            javascriptEnabled: true
-                        }
-                    }
-                    ]
-                },
-                {
-                    test: /\.(css|less)$/,
-                    include: /node_modules/,
-                    use: [{
-                        loader: 'style-loader'
-                    },
-                    {
-                        loader: 'css-loader',
-                        options: {}
-                    },
-                    {
-                        loader: 'postcss-loader',
-                        options: {
-                            ident: 'postcss',
-                            plugins: () => [postcssPresetEnv()]
-                        }
-                    },
-                    {
-                        loader: 'less-loader',
-                        options: {
-                            javascriptEnabled: true,
-                        }
-                    }
-                    ]
-                },
-                {
-                    test: /\.svg(\?v=\d+\.\d+\.\d+)?$/,
-                    use: [
-                        {
-                            loader: 'babel-loader'
-                        },
-                        {
-                            loader: '@svgr/webpack',
-                            options: {
-                                babel: false,
-                                icon: true
-                            }
-                        }
-                    ]
-                },
-                {
-                    test: /\.(png|svg|jpe?g|gif)$/i,
-                    use: [{
-                        loader: 'file-loader',
-                        options: {
-                            limit: 500,
-                            outputPath: 'public/images/',
-                            name: '[name].[hash:8].[ext]'
-                        }
-                    }]
-                },
-                {
-                    test: /\.md$/,
-                    use: [
-                        {
-                            loader: "html-loader"
-                        },
-                        {
-                            loader: "markdown-loader",
-                            options: {
-                                /* your options here */
-                            }
-                        }
-                    ]
-                },
-                {
-                    test: /\.(woff|woff2|eot|ttf|otf)$/,
-                    use: [
-                        'file-loader'
-                    ]
-                }
-            ]
-        },
-        externals: {
-            'react': 'react',
-            'react-dom': 'react-dom'
         }
     });
     compiler.run((err, stats) => { // Stats Object
@@ -191,16 +63,16 @@ function build(dir, opts = {}) {
             console.error(err.stack || err);
             return;
         }
+        lessHandle()
+        // const info = stats.toJson();
 
-        const info = stats.toJson();
+        // if (stats.hasErrors()) {
+        //     console.error(info.errors);
+        // }
 
-        if (stats.hasErrors()) {
-            console.error(info.errors);
-        }
-
-        if (stats.hasWarnings()) {
-            console.warn(info.warnings);
-        }
+        // if (stats.hasWarnings()) {
+        //     console.warn(info.warnings);
+        // }
     })
 }
 
---
+++ src/utils/getWebpackConfig.js
@@ -0,0 +1,162 @@
+const {join} = require('path')
+const postcssPresetEnv = require('postcss-preset-env');
+
+
+const BabelOption = {
+    presets: [
+        [
+            require.resolve('@babel/preset-typescript'), // ts预设
+            {},
+        ],
+        [
+            require.resolve('@babel/preset-env'), // 官方推荐的env预设
+        ],
+        require.resolve('@babel/preset-react')
+        // ...(isBrowser ? [require.resolve('@babel/preset-react')] : []), // 浏览器环境加入react预设
+    ],
+    plugins: [
+        require.resolve('@babel/plugin-proposal-export-default-from'),
+        require.resolve('@babel/plugin-proposal-do-expressions'),
+        require.resolve('@babel/plugin-proposal-class-properties'),
+    ]
+}
+
+
+
+module.exports = function getWebpackConfig(opts = {}) {
+    const {cwd} = opts;
+    return {
+        mode: 'production',
+        module: {
+            rules: [
+                {
+                    test: /\.(js|jsx)$/,
+                    loader: 'babel-loader',
+                    options: BabelOption,
+                    exclude: /node_modules/,
+                },
+                {
+                    test: /\.(ts|tsx)$/,
+                    use: [
+                        {
+                            loader: 'babel-loader',
+                            options: BabelOption,
+                        },
+                        {
+                            loader: 'ts-loader',
+                            options: {
+                                configFile: join(cwd, 'tsconfig.json'),
+                            }
+                        }
+                    ],
+                    exclude: /node_modules/,
+                },
+                {
+                    test: /\.(css|less)$/,
+                    exclude: /node_modules/,
+                    use: [{
+                        loader: 'style-loader'
+                    },
+                    {
+                        loader: 'css-loader',
+                        options: {
+                            sourceMap: true,
+                            modules: { // 适配css-loader2
+                                localIdentName: '[local].[hash:8]'
+                            },
+                        }
+                    },
+                    {
+                        loader: 'postcss-loader',
+                        options: {
+                            ident: 'postcss',
+                            plugins: () => [postcssPresetEnv()]
+                        }
+                    },
+                    {
+                        loader: 'less-loader',
+                        options: {
+                            javascriptEnabled: true
+                        }
+                    }
+                    ]
+                },
+                {
+                    test: /\.(css|less)$/,
+                    include: /node_modules/,
+                    use: [{
+                        loader: 'style-loader'
+                    },
+                    {
+                        loader: 'css-loader',
+                        options: {}
+                    },
+                    {
+                        loader: 'postcss-loader',
+                        options: {
+                            ident: 'postcss',
+                            plugins: () => [postcssPresetEnv()]
+                        }
+                    },
+                    {
+                        loader: 'less-loader',
+                        options: {
+                            javascriptEnabled: true,
+                        }
+                    }
+                    ]
+                },
+                {
+                    test: /\.svg(\?v=\d+\.\d+\.\d+)?$/,
+                    use: [
+                        {
+                            loader: 'babel-loader'
+                        },
+                        {
+                            loader: '@svgr/webpack',
+                            options: {
+                                babel: false,
+                                icon: true
+                            }
+                        }
+                    ]
+                },
+                {
+                    test: /\.(png|svg|jpe?g|gif)$/i,
+                    use: [{
+                        loader: 'file-loader',
+                        options: {
+                            limit: 500,
+                            outputPath: 'public/images/',
+                            name: '[name].[hash:8].[ext]'
+                        }
+                    }]
+                },
+                {
+                    test: /\.md$/,
+                    use: [
+                        {
+                            loader: "html-loader"
+                        },
+                        {
+                            loader: "markdown-loader",
+                            options: {
+                                /* your options here */
+                            }
+                        }
+                    ]
+                },
+                {
+                    test: /\.(woff|woff2|eot|ttf|otf)$/,
+                    use: [
+                        'file-loader'
+                    ]
+                }
+            ]
+        },
+        externals: {
+            'react': 'react',
+            'react-dom': 'react-dom'
+        }
+    }
+}
